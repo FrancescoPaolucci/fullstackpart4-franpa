19bf259cebb509ded4e447776c50559ff7cdcad7
Francesco Paolucci <francescopaolucci55@gmail.com>
2021-09-27 10:02:13 +0300

    ignore

--- BlogPart4/utils/list_helper.js
+++ BlogPart4/utils/list_helper.js
@@ -6,7 +6,14 @@ const sumLikes = (blogs) => {
   return blogs.reduce((total, obj) => obj.likes + total, 0);
 };
 
+const maxLikes = (blogs) => {
+  return blogs.reduce((max, blogs) => {
+    return blogs.likes >= max.likes ? blogs : max;
+  });
+};
+
 module.exports = {
   dummy,
   sumLikes,
+  maxLikes,
 };
